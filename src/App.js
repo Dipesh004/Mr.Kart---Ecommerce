8725637798c847eec298394f3ec7d9ad69b36e50
dipesh <dipeshsharma938@gmail.com>
2022-08-07 14:09:07 +0530

    completed navbar & Updated app name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>   
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: 'Dipesh',
+      city: 'Faridabad',
+      pincode: '121006'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header username={this.state.username} city={this.state.city} pincode={this.state.pincode} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 
-function Header() {
+function Header(props) {
     return (
         <div style={Styles.header} className='header'>
             <span style={Styles.appname}>
                 <span style={Styles.appname1} >Mr.</span>
                 <span style={Styles.appname2} >Kart</span>
             </span>
+            <span style={Styles.username}>
+                <span style={Styles.username1}
+                >Deliver to {props.username}</span>
+                <span style={Styles.username2} ><img style={Styles.locationicon} src="https://cdn-icons.flaticon.com/png/512/2838/premium/2838912.png?token=exp=1659739312~hmac=054bb2edacf226d9c47042d24d7f7dc8" alt="location" /> {props.city} {props.pincode}</span>
+            </span>
             <span><input style={Styles.searchbar} type="search" name="Search" placeholder='Search Products...' id="" /></span>
-            <span><img style={Styles.searchicon} className="searchicon" src="https://cdn-icons-png.flaticon.com/512/64/64673.png" alt="Searchicon" srcset="" /></span>
+            <span><img style={Styles.searchicon} className="searchicon" src="https://cdn-icons-png.flaticon.com/512/64/64673.png" alt="Searchicon" /></span>
+            <span style={Styles.carticon} ><img style={Styles.carticonimage1} src="https://cdn-icons-png.flaticon.com/512/711/711897.png" alt="" />
+            <span style={Styles.carticonimage2} >3</span></span>
         </div>
     )
 
@@ -25,12 +32,12 @@ const Styles = {
         color: 'white',
     },
     appname: {
-        marginLeft: '1.25rem',
-        marginTop:'.25rem'
+        marginLeft: '1.5rem',
+        marginTop: '.25rem'
     },
     appname1: {
         fontSize: '2rem',
-        marginLeft: '2rem',
+        marginLeft: '0rem',
         fontFamily: 'BhuTuka Expanded One',
         position: 'relative',
         top: '-.1rem'
@@ -40,21 +47,56 @@ const Styles = {
         marginLeft: '0rem',
         fontFamily: 'Exo:ital',
         fontWeight: '600'
+    }, username: {
+        fontFamily: 'arial',
+        marginLeft: '1rem',
+        marginTop: '.6rem',
+        display: 'flex',
+        flexDirection: 'column'
+    }, username1: {
+        fontFamily: 'arial',
+        marginLeft: '0.2rem',
+        fontSize: '.75rem'
+    }, username2: {
+        fontFamily: 'arial',
+        marginLeft: '1rem',
+        marginTop: '-.05rem'
+    }, locationicon: {
+        height: '17.5px',
+        width: '17.5px'
+
     },
     searchbar: {
         fontFamily: 'arial',
         width: '32.5rem',
         height: '2.25rem',
-        marginLeft: '14rem',
+        marginLeft: '6rem',
         marginTop: '.65rem',
-        borderRadius:'.4rem'
+        borderRadius: '.4rem',
+        backgroundColor:'white',
+        border:'white'
     },
-    searchicon:{
+    searchicon: {
+        height: '30px',
+        width: '30px',
+        position: 'relative',
+        top: '.9rem',
+        left: '.5rem',
+    },
+    carticon:{
+        marginTop:'.9rem',
+        marginLeft:'20.5rem'
+    },
+    carticonimage1:{
         height:'30px',
         width:'30px',
-        position:'relative',
-        top:'.9rem',
-        left:'.5rem'
+    },
+    carticonimage2:{
+       padding:'.2rem',
+       position:'relative',
+       top:'-1.7rem',
+       left:'-1.25rem',
+       color:'black'
     }
 }
 export default Header;
